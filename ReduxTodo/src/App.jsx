f46833f1c69b25a2a6a855f47a9f36df5e939b87
amanmakwana-4 <ma5066770@gmail.com>
2025-11-24 21:42:46 +0530

    feat: initialize Redux Todo application with React, Redux Toolkit, and Tailwind CSS
    - Added package.json with dependencies for React, Redux Toolkit, Tailwind CSS, and development tools.
    - Included Vite configuration for building and serving the application.
    - Created main application structure with App component, including AddTodo and Todos components.
    - Implemented Redux store and todoSlice for managing todo state.
    - Developed AddTodo component for adding new todos and Todos component for displaying the list of todos.
    - Styled components using Tailwind CSS for a responsive design.
    - Added SVG assets for branding and icons.

---
+++ ReduxTodo/src/App.jsx
@@ -0,0 +1,20 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full bg-gray-900 h-screen py-10 px-100 justify-center items-center">
+      <h1>Learn Redux Toolkit- with Todos</h1>
+      <div className="flex justify-center items-center">
+        <AddTodo />
+        </div>
+         <Todos />
+      </div>
+    </>
+  )
+}
+
+export default App
